f356fbf1b279cf03973303995fb86d0eea219c54
Roy Bailey <rbailey@silvertouch.co.uk>
2015-11-17 21:02:52 +0000

    Minor mods to re hook home page

---
+++ controllers/homeController.js
@@ -0,0 +1,11 @@
+﻿(function (homeController) {
+
+  homeController.init = function (app) {
+
+    app.get("/", function (req, res) {
+        res.render("index", { title: "UK Company Search", BasicAuthHeader : process.env.CH_BASIC_AUTH });
+    });
+
+  };
+
+})(module.exports);
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 ﻿(function (controllers) {
 
+  var homeController = require("./homeController");
   var companySearchController = require("./api/companySearchController");
 
   controllers.init = function (app) {
+    homeController.init(app);
     companySearchController.init(app);
   };
 
